61f234a9c4af0028cb861b1106feb1c228fdb769
Artem_Dubinskiy <ar7t7em7@yandex.ru>
2026-03-10 15:59:17 +0300

    добавление закладок на главный экран

--- src/application/SimpleWindowApp.java
+++ src/application/SimpleWindowApp.java
@@ -1,8 +1,9 @@
 package application;
 
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
@@ -22,18 +23,25 @@ public class SimpleWindowApp extends Application {
 	}
 
 	private void startWindow(Stage stage) {
-		Group root = new Group();
-		Scene scene = new Scene(root, Color.AZURE);
+		TabPane tabPane = createTabPane();
 		
-		Image icon = new Image("icon.png");
-
-		
-		stage.getIcons().add(icon);
+		Scene scene = new Scene(tabPane, Color.AZURE);
+				
+		stage.getIcons().add(new Image("icon.png"));
 		stage.setTitle("Автообмен");
 		stage.setScene(scene);
 		stage.setWidth(1024);
 		stage.setHeight(768);
 		
 		stage.show();
+	}
+
+	private TabPane createTabPane() {
+		TabPane tabPane = new TabPane();
+        Tab tab1 = new Tab("Главная");
+        Tab tab2 = new Tab("Настройки");
+        Tab tab3 = new Tab("О программе");
+        tabPane.getTabs().addAll(tab1, tab2, tab3);
+		return tabPane;
 	}
 }
